e5d0a6fe723fdd1babf7b6bad7a062b5491f5726
adnan wahab <mail@adnanwahab.com>
2020-12-28 22:53:51 -0500

    penis

--- api/src/functions/mongo.js
+++ api/src/functions/mongo.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const client = new MongoClient(url, { useUnifiedTopology: true });
 
 
-export const handler =  async (event, context, ) => {
+export const handler =  async (event, context) => {
   // connect to your cluster
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
@@ -27,43 +27,6 @@ export const handler =  async (event, context, ) => {
   }
 
 
-
-
-  // client.connect(function(err) {
-    // console.log(err)
-    // console.log("Connected successfully to server");
-
-    // const collection = client.db('test').collection('cars')
-    // const query = { name: 'Tesla' }
-    // let data =  await collection.findOne(query)
-    // client.close();
-
-      // callback(null, {
-      //   statusCode: 200,
-      //   body: JSON.stringify(data)
-      // })
-
-
-
-  // });
-
-
-
-
-    // const collection = client.collection('cars');
-    // let data =  collection.findOne(query).then((data) => {
-
-    //   callback(null, {
-    //     statusCode: 200,
-    //     headers: { 'Content-Type': 'application/json ' },
-    //     body: JSON.stringify({
-    //       data: {
-    //         result: data,
-    //         found: true
-    //       }
-    //     }),
-    //   })
-    // })
 
 
 
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { StaticMap } from 'react-map-gl'
 import DeckGL from 'deck.gl'
 import { LayerControls, HEXAGON_CONTROLS } from 'src/components/controls'
-import ListView from 'src/components/list-view.js'
+import LegendView from 'src/components/LegendView.js'
 import styled from 'styled-components'
 import { layers } from 'src/components/layers'
 
@@ -13,17 +13,17 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
   bearing: 0,
   width: 960,
-  height: 500
+  height: 500,
 }
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = // process.env.MapboxAccessToken; // eslint-disable-line
-'pk.eyJ1IjoiYXdhaGFiIiwiYSI6ImNpenExZHF0ZTAxMXYzMm40cWRxZXY1d3IifQ.TdYuekJQSG1eh6dDpywTxQ'
+  'pk.eyJ1IjoiYXdhaGFiIiwiYSI6ImNpenExZHF0ZTAxMXYzMm40cWRxZXY1d3IifQ.TdYuekJQSG1eh6dDpywTxQ'
 
 const Legend = styled.section`
-display: fixed;
+  display: fixed;
 `
-function Root () {
+function Root() {
   const [selection, setSelection] = useState(0)
   const [layer, setLayer] = useState([])
 
@@ -51,20 +51,24 @@ function Root () {
 
   return (
     <div>
-      <ListView selectedIndex={selection} onClick={_selectLayer} />
+      <LegendView selectedIndex={selection} onClick={_selectLayer} />
 
-      {false && (<LayerControls
-        settings={this.state.settings}
-        propTypes={HEXAGON_CONTROLS}
-        onChange={settings => this._updateLayerSettings(settings)}
-                 />)}
+      {false && (
+        <LayerControls
+          settings={this.state.settings}
+          propTypes={HEXAGON_CONTROLS}
+          onChange={(settings) => this._updateLayerSettings(settings)}
+        />
+      )}
 
       <DeckGL initialViewState={INITIAL_VIEW_STATE} controller layers={[layer]}>
-        <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} mapStyle='mapbox://styles/mapbox/dark-v9' />
+        <StaticMap
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+        />
       </DeckGL>
 
       <Legend />
-
     </div>
   )
 }
