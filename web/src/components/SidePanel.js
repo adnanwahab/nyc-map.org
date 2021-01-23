5460bffb28b5dc2ff15c0f2a83dfee69f573a759
adnan wahab <mail@adnanwahab.com>
2021-01-23 13:36:01 -0500

    redesign

--- web/src/components/SidePanel.js
+++ web/src/components/SidePanel.js
@@ -35,28 +35,70 @@ const List = styled.section`
   padding: 0.25em 1em;
 `
 // The above changes the color for the legend.
-const Blurb = styled.section`
-  font-size: 10px;
+const SidePanel = styled.section`
+  line-height: 21px;
+  font-size: 16px;
   margin-top: 5px;
-  padding: 0.25em 1em;
+  padding: 0px;
   position: fixed;
   z-index: 1100;
   background: white;
   background-opacity: 0.9;
   color: black;
   overflow-y: auto;
+  box-shadow: 0 0 4px 2px rgba(0, 0, 0, 0.2);
 `
+
+const SubHeader = styled.section`
+  padding: 0px 1.5rem;
+  border-color: rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid #333;
+  border-color: rgba(0, 0, 0, 0.1);
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  font-weight: 600;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+`
+
+const Logo = styled.img`
+  padding-right: 10px;
+`
+
+const LinkTitle = styled.span`
+  cursor: pointer;
+
+  &:hover {
+    color: palevioletred;
+  }
+`
+
+const openModal = () => {
+  alert(
+    'Find the best appartment to live in using the best data sets!!! fuck brokers they are lying scum trash!!!! '
+  )
+}
 
 const ListView = (props) => {
   const [checked, setChecked] = useState(false)
   return (
-    <Blurb>
-      <h1 className="under-line">Appartment Explorer</h1>
-      <p className="intro-paragraph">Choose the best place to live using artificial intelligence</p>
-      <h4 className="vis-layer">Visualization Layers</h4>
+    <SidePanel>
+      <SubHeader>
+        <Logo src="/favicon.png" />
+        <LinkTitle onClick={openModal}>Crib Finder</LinkTitle>
+      </SubHeader>
+      {/* <p className="intro-paragraph">
+        Choose the best place to live using artificial intelligence
+      </p> */}
+      <SubHeader>Complaints</SubHeader>
+      <SubHeader>Places</SubHeader>
+      <SubHeader>Suitability</SubHeader>
+      <SubHeader>Crimes</SubHeader>
+      <SubHeader>Demographics</SubHeader>
+
       <List onClick={props.onClick}>{buildListItems(props.selectedIndex)}</List>
       <p>{props.exposition}</p>
-    </Blurb>
+    </SidePanel>
   )
 }
 
