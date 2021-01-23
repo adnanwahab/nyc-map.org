fc7c8f7b731e94554c6254afda2d7bbb47c7b1d4
Horatiu Mitrea <horatiu.mitrea@gmail.com>
2021-01-22 21:43:12 -0500

    Making Blurb Looking Better

--- web/src/components/LegendView.js
+++ web/src/components/LegendView.js
@@ -34,18 +34,20 @@ const List = styled.section`
   margin: 0 1em;
   padding: 0.25em 1em;
 `
-
 const Blurb = styled.section`
-  border-radius: 3px;
-  margin: 0 1em;
+  border-radius: 50px;
+  font-size: 10px;
+  margin-top: 5px;
   padding: 0.25em 1em;
   position: fixed;
-  top: 50px;
-  z-index: 1000;
-  background: rgb(41, 50, 60);
+  top: 10px;
+  z-index: 1100;
+  background: rgb(130, 221, 234);
   background-opacity: 0.9;
-  color: lightgrey;
+  color: black;
+  overflow-y:auto
 `
+// The above changes the color for the legend. Make it look nice
 
 const ListView = (props) => {
   const [checked, setChecked] = useState(false)
