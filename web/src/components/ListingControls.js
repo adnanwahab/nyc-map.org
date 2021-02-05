24e5fe78cd052da54b23602365540dab86ca966f
adnan wahab <mail@adnanwahab.com>
2021-02-05 13:44:53 -0600

    beautify modal

--- web/src/components/ListingControls.js
+++ web/src/components/ListingControls.js
@@ -139,7 +139,7 @@ const SidePanel = styled.section`
   font-weight: 500;
   color: black;
 
-  height: 200px;
+  height: 250px;
   width: 350px;
   overflow: scroll;
 `
@@ -212,20 +212,22 @@ const ListingControls = (props) => {
     <>
       <div
         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-        onClick={() => setShowModal(false)}
+        //onClick={() => setShowModal(false)}
       >
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
-            <form class="m-4 flex">
+            <form class="m-4 flex" onSubmit={() => {console.log('submit')}}>
     	<input class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="your@mail.com"/>
 		<button class="px-8 rounded-r-lg bg-purple-400  text-gray-800 font-bold p-4 uppercase border-purple-500 border-t border-b border-r">Subscribe</button>
 	</form>
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div
+      onClick={() => setShowModal(false)}
+      className="opacity-25 fixed inset-0 z-40 bg-black"></div>
     </>)
 
   return (
@@ -259,8 +261,8 @@ const ListingControls = (props) => {
           onChange={nextValues => setPriceRange(nextValues)}
         />
       </Box>
-
-      {/* <Box size='small' direction='row' align='center' pad='medium' gap='small'>
+{/*
+      <Box size='small' direction='row' align='center' pad='medium' gap='small'>
         <button onClick={()=> setShowModal(! showModal)} className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>
           Get Email Alerts about this Search
         </button>
