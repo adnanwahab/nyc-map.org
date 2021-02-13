7900b6237b950809c8bb4666c99fa5f42911d21b
adnan wahab <mail@adnanwahab.com>
2021-02-13 17:11:22 -0600

    finish accordion dynamic

--- web/src/components/accordion.js
+++ web/src/components/accordion.js
@@ -6,37 +6,21 @@ import PlaceControls from './PlaceControls'
 
 const Accordion = (fixme) => {
   let props = fixme.props
+  console.log(props)
+
+  let controls = [<ComplaintControls setLayer={props.setLayer}/>, <CommuteDistanceControls setLayer={props.setLayer}/>,  <PlaceControls setLayer={props.setLayer}/>, <SuitabilityControls setLayer={props.setLayer}/>]
+
+  let list = ["311-complaints", "commute-distance", "places","suitability"].map((children, idx) => {
+    return <div className="tab w-full overflow-hidden border-t">
+    <input className="absolute opacity-0" id={children} type="radio" name="tabs2" />
+    <label className="block p-5 leading-normal cursor-pointer" for={children}>{children}</label>
+    <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+      {controls[idx]}
+    </div>
+    </div>
+  })
 return (<div className="shadow-md">
-<div className="tab w-full overflow-hidden border-t">
-<input className="absolute opacity-0" id="tab-single-one" type="radio" name="tabs2" />
-<label className="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
-<div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-{/* <CommuteDistanceControls setLayer={props.setLayer} /> */}
-</div>
-</div>
-<div className="tab w-full overflow-hidden border-t">
-<input className="absolute opacity-0" id="tab-single-three" type="radio" name="tabs2" />
-<label className="block p-5 leading-normal cursor-pointer" for="tab-single-three">Label One</label>
-<div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-<CommuteDistanceControls setLayer={props.setLayer} />
-</div>
-</div>
-<div className="tab w-full overflow-hidden border-t">
-<input className="absolute opacity-0" id="tab-single-four" type="radio" name="tabs2" />
-<label className="block p-5 leading-normal cursor-pointer" for="tab-single-four">Label Two</label>
-<div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-  <PlaceControls setLayer={props.setLayer} />
-</div>
-</div>
-<div className="tab w-full overflow-hidden border-t">
-<input className="absolute opacity-0" id="tab-single-five" type="radio" name="tabs2" />
-<label className="block p-5 leading-normal cursor-pointer" for="tab-single-five">Label Three</label>
-<div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-  <SuitabilityControls setLayer={props.setLayer}/>
-</div>
-</div>
+  {list}
 </div>)
 }
 
